b7dd5fa605fa62e8755427aff6a151acd4967efe
BarndoBoi <brandon.a.eastep@gmail.com>
2022-10-03 12:23:47 -0400

    Experimental Checkers Screen
    Added view for checkers game that takes user input for their player name.
    Cleaned up unused functions and removed incorrect settings button styling

--- App.js
+++ App.js
@@ -8,91 +8,76 @@
  * @flow strict-local
  */
 
-import React, {useState} from 'react';
-import type { Node } from 'react';
-import {
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    Button,
-} from 'react-native';
+ import React, {useState} from 'react';
+ import type { Node } from 'react';
+ import {
+     StatusBar,
+     StyleSheet,
+     Text,
+     View,
+     TextInput,
+     Button,
+ } from 'react-native';
+ 
+ //Four game buttons displayed
+ //Home screen with settings button
+ //Settings button opens a new screen with adjustments in it
+ //Current settings screen will only have placeholder text
+ //Each game button navigates to a placeholder screen
+ //Placeholder text + description
+ //And Back to Home button
 
-//Four game buttons displayed
-//Home screen with settings button
-//Settings button opens a new screen with adjustments in it
-//Current settings screen will only have placeholder text
-//Each game button navigates to a placeholder screen
-//Placeholder text + description
-//And Back to Home button
+ 
+ const App: () => Node = () => {
 
-function nextScreen(screenName) {
-
-}
-
-const App: () => Node = () => {
-
-    const backgroundStyle = {
-    };
-    const [menuIsVisible, setMenuVisible] = useState(true);
-
-    return (
-        <View>
-            <StatusBar></StatusBar>
-            <View style={[styles.menuView, { display: menuIsVisible ? 'flex' : 'none' }]}>
-                <Text style={styles.titleText}>
-                    Ye Old Games
-                </Text>
-                <Button
-                    title='Chess'
-                    onPress={nextScreen('Chess')}
-                    style={styles.gameButton}
-                    accessibilityLabel='Chess'>
-                </Button>
-                <Button
-                    title='Checkers'
-                    onPress={nextScreen('Checkers')}
-                    style={styles.gameButton}
-                    accessibilityLabel='Checkers'>
-                </Button>
-                <Button
-                    title='Solitaire'
-                    onPress={nextScreen('Solitaire')}
-                    style={styles.gameButton}
-                    accessibilityLabel='Solitaire'>
-                </Button>
-                <Button
-                    title='Fancy Chess'
-                    onPress={nextScreen('Fancy Chess')}
-                    style={[styles.gameButton, { disabled: true }]}
-                    accessibilityLabel='Fancy Chess'>
-                </Button>
-            </View>
-            <Button
-                 title='Settings'
-                 onPress={() => setMenuVisible(!menuIsVisible)}
-                 style={styles.settingsButton}>
+     const [mainMenuIsVisible, setMainMenuVisible] = useState(true);
+ 
+     const[checkersScreenIsVisible, setCheckersScreenVisible] = useState(false);
+     const [playerName, setPlayerName] = useState('Player');
+ 
+     return (
+         <View>
+             <StatusBar></StatusBar>
+             <View style={[styles.menuView, { display: mainMenuIsVisible ? 'flex' : 'none' }]}>
+                 <Text style={styles.titleText}>
+                     Ye Old Games
+                 </Text>
+                 <Button
+                     title='Checkers'
+                     onPress={() => {setMainMenuVisible(false); setCheckersScreenVisible(true);}}
+                     style={styles.gameButton}
+                     accessibilityLabel='Checkers'>
                  </Button>
-         </View>
-     );
- };
-
- const styles = StyleSheet.create({
-     menuView: {
-         justifyContent: 'center',
-     },
-     gameButton: {
-         color: "aqua",
-     },
-     settingsButton: {
-         color: "gold",
-     },
-     titleText: {
-         fontSize: 30,
-         fontWeight: '600',
-     },
- });
-
- export default App;
+             </View>
+             <View style={{display: checkersScreenIsVisible ? 'flex' : 'none'}}>
+                 <TextInput
+                     value={playerName}
+                     placeholder='Player'
+                     onChangeText={setPlayerName}/>
+                 <Text>Hello, {playerName}</Text>
+                 <Button title='Go To Menu' accessibilityLabel='Go To Menu' onPress={() => {setCheckersScreenVisible(false); setMainMenuVisible(true);}}></Button>
+             </View>
+             <Button
+                  title='Settings'
+                  onPress={() => setMainMenuVisible(!mainMenuIsVisible)}>
+                 </Button>
+          </View>
+      );
+  };
+ 
+  const styles = StyleSheet.create({
+      menuView: {
+          justifyContent: 'center',
+      },
+      gameButton: {
+          color: "aqua",
+      },
+      titleText: {
+          fontSize: 30,
+          fontWeight: '600',
+          textAlign: 'center'
+      },
+  });
+ 
+  export default App;
+ 
