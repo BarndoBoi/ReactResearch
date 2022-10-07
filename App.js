bcca068cef24520897231d3081bc7cdc86e92419
BarndoBoi <brandon.a.eastep@gmail.com>
2022-10-07 12:15:12 -0400

    App.js styling
    Expanded style sheets and added spacer views in the menu view

--- App.js
+++ App.js
@@ -21,15 +21,19 @@
      Image,
  } from 'react-native';
  
- //Four game buttons displayed
- //Home screen with settings button
- //Settings button opens a new screen with adjustments in it
- //Current settings screen will only have placeholder text
- //Each game button navigates to a placeholder screen
- //Placeholder text + description
- //And Back to Home button
- 
+ /*
+ base for our game:
+ Four game buttons displayed
+ Home screen with settings button
+ Settings button opens a new screen with adjustments in it
+ Current settings screen will only have placeholder text
+ Each game button navigates to a placeholder screen
+ Placeholder text + description
+ And Back to Home button
+ This is a simplified version to get started
+ */
  const App: () => Node = () => {
+    // state declerations, move to own component later
      const [mainMenuIsVisible, setMainMenuVisible] = useState(true);
      const [settingsScreenVisible, setSettingsScreenVisible] = useState(false);
      const [checkersScreenIsVisible, setCheckersScreenVisible] = useState(false);
@@ -39,30 +43,36 @@
      return (
          <View>
              <StatusBar></StatusBar>
+             {/* main menu view screen */}
              <View style={[styles.menuView, { display: mainMenuIsVisible ? 'flex' : 'none' }]}>
                  <Text style={styles.titleText}>
                      Ye Old Games
                  </Text>
+                 <View style={{height:40}}></View>
                  <Button
                      title='Checkers'
                      onPress={() => {setMainMenuVisible(false); setCheckersScreenVisible(true);}}
                      style={styles.gameButton}
                      accessibilityLabel='Checkers'>
                  </Button>
+                 <View style={{height:40}}></View>
                  <Button
+                 style={styles.gameButton}
                   title='Settings'
                   onPress={() => {setMainMenuVisible(false); setSettingsScreenVisible(true)}}
                   style={styles.settingsButton}>
                  </Button>
              </View>
+             {/* checkers view screen */}
              <View style={{display: checkersScreenIsVisible ? 'flex' : 'none'}}>
                  <TextInput
                      value={playerName}
                      placeholder='Player'
                      onChangeText={setPlayerName}/>
                  <Text>Hello, {playerName}</Text>
-                 <Button title='Go To Menu' accessibilityLabel='Go To Menu' onPress={() => {setCheckersScreenVisible(false); setMainMenuVisible(true);}}></Button>
+                 <Button style={styles.gameButton} title='Go To Menu' accessibilityLabel='Go To Menu' onPress={() => {setCheckersScreenVisible(false); setMainMenuVisible(true);}}></Button>
              </View>
+             {/* settings view screen */}
              <View style={{display: settingsScreenVisible ? 'flex' : 'none'}}>
                  <Text style={styles.settingsHeader}>Settings</Text>
                  <Text>Mute Audio
@@ -70,30 +80,34 @@
                      onValueChange={setMuteAudio}
                      value={muteAudio}/>
                  </Text>
-                 <Button title='Go To Menu' accessibilityLabel='Go To Menu' onPress={() => {setSettingsScreenVisible(false); setMainMenuVisible(true);}}></Button>
+                 <Button style={styles.gameButton} title='Go To Menu' accessibilityLabel='Go To Menu' onPress={() => {setSettingsScreenVisible(false); setMainMenuVisible(true);}}></Button>
              </View>
           </View>
       );
   };
- 
-  const styles = StyleSheet.create({
-      menuView: {
-          justifyContent: 'center',
-      },
-      gameButton: {
-          color: "aqua",
-      },
-      titleText: {
-          fontSize: 30,
-          fontWeight: '600',
-          textAlign: 'center'
-      },
-      settingsHeader: {
-          textAlign: 'center',
-          fontSize:35,
-          fontWeight:'bold',
-      },
-  });
- 
+ // style sheet instead of defaults
+ const styles = StyleSheet.create({
+    menuView: {
+        justifyContent: 'center',
+        margin: 10,
+        padding: 25,
+    },
+    gameButton: {
+        margin: 10,
+        padding: 25,
+        borderwidth: 5,
+        borderRadius: 20,
+    },
+    titleText: {
+        fontSize: 30,
+        fontWeight: '600',
+        textAlign: 'center'
+    },
+    settingsHeader: {
+        textAlign: 'center',
+        fontSize:35,
+        fontWeight:'bold',
+    },
+});
+
   export default App;
- 
